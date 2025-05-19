8256f5640fe0fd503251f49983d2992cd4905898
Gabrielseilhe <168229694+gaberilseilhe@users.noreply.github.com>
2025-05-19 17:36:23 -0300

    Add files via upload

--- constants/agenda.tsx
+++ constants/agenda.tsx
@@ -5,37 +5,37 @@ const agenda = [
         hora: "12:00"
     },
     {
-        id_agenda: 1,
+        id_agenda: 2,
         dia: 6,
         hora: "15:00"
     },
     {
-        id_agenda: 1,
+        id_agenda: 3,
         dia: 3,
         hora: "12:30"
     },
     {
-        id_agenda: 1,
+        id_agenda: 4,
         dia: 4,
         hora: "08:30"
     },
     {
-        id_agenda: 1,
+        id_agenda: 5,
         dia: 5,
         hora: "12:25"
     },
     {
-        id_agenda: 1,
+        id_agenda: 6,
         dia: 3,
         hora: "16:30"
     },
     {
-        id_agenda: 1,
+        id_agenda: 7,
         dia: 6,
         hora: "12:30"
     },
     {
-        id_agenda: 1,
+        id_agenda: 8,
         dia: 5,
         hora: "10:30"
     },
--- constants/servicos.tsx
+++ constants/servicos.tsx
@@ -1,10 +1,10 @@
 const servicos = [
-    {id_servico:1, nome_servico: "Corte de cabelo"},
-    {id_servico:2, nome_servico: "Barba"},
-    {id_servico:3, nome_servico: "Corte e barba"},
-    {id_servico:4, nome_servico: "Sobrancelha"},
-    {id_servico:5, nome_servico: "Pigmentação"},
-    {id_servico:6, nome_servico: "Luzes"},
+    {id_servico:1, nome_servico: "Corte"},
+    {id_servico:2, nome_servico: "Escova"},
+    {id_servico:3, nome_servico: "Tratamento capilar"},
+    {id_servico:4, nome_servico: "Progressiva"},
+    {id_servico:5, nome_servico: "Mechas-Luzes-Reflexos"},
+    {id_servico:6, nome_servico: "Sobrancelha"},
 ]
 
 export const Servicos = function(){
